6b54e6cbee58091ae6fb483d17130b54994cd41b
awaji-daisuke <awaji_daisuke@intec.co.jp>
2020-10-26 23:32:15 +0900

    horizontal scroll example

--- screens/setting/SettingScreen.tsx
+++ screens/setting/SettingScreen.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { StyleSheet } from "react-native";
-import { Divider, Layout, Text } from "@ui-kitten/components";
+import { Divider, Layout, Text, Toggle } from "@ui-kitten/components";
 import { SettingScreenProps } from "../../navigation/Setting.navigator";
 import { Toolbar } from "../../components/Toolbar";
 import {
@@ -10,30 +10,98 @@ import {
 } from "../../components/SafeAreaLayout";
 import { MenuIcon } from "../../assets/icons";
 import { APP_NAME } from "../../constants";
+import { ScrollView } from "react-native-gesture-handler";
+import { Category } from "./Category";
 
 export const SettingScreen = (
   props: SettingScreenProps
-): SafeAreaLayoutElement => (
-  <SafeAreaLayout style={styles.safeArea} insets={SaveAreaInset.TOP}>
-    <Toolbar
-      title={APP_NAME}
-      backIcon={MenuIcon}
-      onBackPress={props.navigation.toggleDrawer}
-    />
-    <Divider />
-    <Layout style={styles.container}>
-      <Text category="h1">SETTING ⚙</Text>
-    </Layout>
-  </SafeAreaLayout>
-);
+): SafeAreaLayoutElement => {
+  const [activeChecked, setActiveChecked] = React.useState(true);
+
+  const onActiveCheckedChange = (isChecked) => {
+    setActiveChecked(isChecked);
+  };
+
+  return (
+    <SafeAreaLayout style={styles.safeArea} insets={SaveAreaInset.TOP}>
+      <Toolbar
+        title={APP_NAME}
+        backIcon={MenuIcon}
+        onBackPress={props.navigation.toggleDrawer}
+      />
+      <Divider />
+      <Layout style={styles.container}>
+        <Text category="h1">SETTING ⚙</Text>
+        <Layout style={styles.horizontalScroll}>
+          <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
+            <Category name="Profile" imageUri="https://picsum.photos/200/300" />
+            <Category
+              name="Notification"
+              imageUri="https://picsum.photos/200/300/?blur=1"
+            />
+            <Category
+              name="Siri"
+              imageUri="https://picsum.photos/200/300/?blur=3"
+            />
+          </ScrollView>
+        </Layout>
+      </Layout>
+      <ScrollView
+        style={{
+          paddingLeft: 20,
+          paddingTop: 10,
+        }}
+      >
+        <Layout style={{ alignItems: "flex-start" }}>
+          <Layout style={{ marginTop: 10, marginBottom: 10 }}>
+            <Toggle checked={activeChecked} onChange={onActiveCheckedChange}>
+              {activeChecked ? "Active" : "Disabled"}
+            </Toggle>
+          </Layout>
+          <Layout style={{ marginTop: 10, marginBottom: 10 }}>
+            <Toggle disabled={true}>Disabled</Toggle>
+          </Layout>
+          <Layout style={{ marginTop: 10, marginBottom: 10 }}>
+            <Toggle disabled={true}>Disabled</Toggle>
+          </Layout>
+          <Layout style={{ marginTop: 10, marginBottom: 10 }}>
+            <Toggle disabled={true}>Disabled</Toggle>
+          </Layout>
+          <Layout style={{ marginTop: 10, marginBottom: 10 }}>
+            <Toggle disabled={true}>Disabled</Toggle>
+          </Layout>
+          <Layout style={{ marginTop: 10, marginBottom: 10 }}>
+            <Toggle disabled={true}>Disabled</Toggle>
+          </Layout>
+          <Layout style={{ marginTop: 10, marginBottom: 10 }}>
+            <Toggle disabled={true}>Disabled</Toggle>
+          </Layout>
+          <Layout style={{ marginTop: 10, marginBottom: 10 }}>
+            <Toggle disabled={true}>Disabled</Toggle>
+          </Layout>
+          <Layout style={{ marginTop: 10, marginBottom: 10 }}>
+            <Toggle disabled={true}>Disabled</Toggle>
+          </Layout>
+          <Layout style={{ marginTop: 10, marginBottom: 10 }}>
+            <Toggle checked={true} disabled={true}>
+              Checked Disabled
+            </Toggle>
+          </Layout>
+        </Layout>
+      </ScrollView>
+    </SafeAreaLayout>
+  );
+};
 
 const styles = StyleSheet.create({
   safeArea: {
     flex: 1,
   },
   container: {
-    flex: 1,
-    justifyContent: "center",
     alignItems: "center",
+  },
+  horizontalScroll: {
+    height: 130,
+    marginTop: 20,
   },
 });
